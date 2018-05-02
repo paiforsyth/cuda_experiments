86560608768506419ba6248f89659b2649293d1c
paiforsyth <paiforsyth@hotmail.com>
2018-05-02 19:27:50 -0400

    <<

--- ch13_quadtree/bounding_box_test.cu
+++ ch13_quadtree/bounding_box_test.cu
@@ -6,6 +6,6 @@ int main(){
     float2 p2 = make_float2(10.0f, 10.0f);
 
     std::cout << "Does point 1 lie in the box? " << box.contains(p1) <<"" << std::endl;
-    std::cout << "Does point 2 lie in the box? " <<< box.contains(p2) <<"" << std::endl;
+    std::cout << "Does point 2 lie in the box? " << box.contains(p2) <<"" << std::endl;
 
 }
