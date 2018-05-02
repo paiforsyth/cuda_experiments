dc21149e75fad27e7828c5c71eae2f84d10a8796
paiforsyth <paiforsyth@hotmail.com>
2018-05-02 18:40:43 -0400

    add void

--- ch13_quadtree/points.cu
+++ ch13_quadtree/points.cu
@@ -8,7 +8,7 @@ __host__ __device__ __forceinline__ float2 Points::get_point(int idx) const{
     return make_float2(m_x[idx], m_y[idx]);
 }
 
- __host__ Points::print_point(int idx) const{
+ __host__ void Points::print_point(int idx) const{
    float2 p = get_point(idx);
    cout << "x: " <<  p.x << " y:" << p.y;
  }
