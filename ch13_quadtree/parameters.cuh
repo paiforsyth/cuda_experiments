7c8f686e1c2c795649a777143becb604e29ed6d2
paiforsyth <paiforsyth@hotmail.com>
2018-05-04 14:31:37 -0400

    m

--- ch13_quadtree/parameters.cuh
+++ ch13_quadtree/parameters.cuh
@@ -8,7 +8,7 @@ class Parameters {
 
     const int max_depth;
 
-    const int min_ponts_per_node;
+    const int min_points_per_node;
     __host__ __device__  Parameters(int max_depth, int min_points_per_node);
 
     __host__ __device__ Parameters(const Parameters& params, bool);//constructor that changes values for next iteration.  Role of bool seems to be merely to distinguish this from the default copy constructor
