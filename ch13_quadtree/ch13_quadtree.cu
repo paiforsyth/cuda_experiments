fa330c8c55296fb9397794dd677dec0d3c90eafc
paiforsyth <paiforsyth@hotmail.com>
2018-05-04 17:07:24 -0400

    ;

--- ch13_quadtree/ch13_quadtree.cu
+++ ch13_quadtree/ch13_quadtree.cu
@@ -164,7 +164,7 @@ __global__ void build_quad_tree_kernel(QuadTreeNode* nodes, Points* points, Para
 }
 
 class RNG_functor{
-    int ip
+    int ip;
     public:
         RNG(int init_p){  
             ip = init_p; 
