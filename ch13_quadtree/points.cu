9e2cd137bf6eca296c23ddc36a114c5fd65618ad
paiforsyth <paiforsyth@hotmail.com>
2018-05-04 14:38:10 -0400

    points::

--- ch13_quadtree/points.cu
+++ ch13_quadtree/points.cu
@@ -12,12 +12,12 @@ __host__ __device__ __forceinline__ float2 Points::get_point(int idx) const{
    float2 p = get_point(idx);
    std::cout << "x: " <<  p.x << " y:" << p.y << std::endl;
  }
- __host__ __device__ __forceinline__ void set_point(int idx, const float2& p){
+ __host__ __device__ __forceinline__ void Points::set_point(int idx, const float2& p){
     m_x[idx] = p.x;
     m_y[idx] = p.y; 
 }
 //set the actual pointers
-__host__ __device__ __forceinline__ void set(float* x , float* y){
+__host__ __device__ __forceinline__ void Points::set(float* x , float* y){
     m_x= x;
     m_y= y;
 }
