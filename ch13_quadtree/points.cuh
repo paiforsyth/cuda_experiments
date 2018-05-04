3c9440b9c92c2bbde8772426168748bc8749dc20
paiforsyth <paiforsyth@hotmail.com>
2018-05-04 19:10:16 -0400

    print

--- ch13_quadtree/points.cuh
+++ ch13_quadtree/points.cuh
@@ -10,7 +10,7 @@ class Points{
 
     __host__ __device__  float2 get_point(int idx) const;
 
-    __host__ void print_point(int idx) const;
+    __host__ void print_point_d_2_h(int idx) const;
 
     __host__ __device__  void set_point(int idx, const float2& p);
     __host__ __device__  void set(float* x , float* y); 
